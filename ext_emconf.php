7b1b73bdb51286d39d20414baf85d3ac1e619228
Philipp Gampe <philipp.gampe@typo3.org>
2013-05-24 20:04:09 +0200

    [INIT] Add basic extension skeleton

---
+++ ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pgampe_bonnvolley".
+ *
+ * Auto generated 06-04-2013 20:58
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Extend Backend Modules',
+	'description' => 'This extension shows howto easily extend extbase based backend modules. This example shows the usergroups of backend users in the list view of the backend users module.',
+	'category' => 'backend',
+	'author' => 'Philipp Gampe',
+	'author_email' => 'philipp.gampe@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => '',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0.0-6.2.99',
+			'beuser' => '6.0.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'suggests' => 
+	array (
+	),
+);
+
+?>
